30128bce3011a7cc09857f94eca421aeff1d1d45
juanOcampo31 <juanfelipe31@gmail.com>
2026-06-24 12:05:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finanzas-v1782309367';
+const CACHE = 'finanzas-v1782320687';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
